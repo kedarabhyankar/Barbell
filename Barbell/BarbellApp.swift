6c4f69c8f717e26175390bd834e4074d2474a6ef
Kedar Abhyankar <krabhyankar@gmail.com>
2024-06-12 10:43:12 -0700

    Initial Commit

---
+++ Barbell/BarbellApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarbellApp.swift
+//  Barbell
+//
+//  Created by Kedar Abhyankar on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BarbellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Barbell/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Barbell
+//
+//  Created by Kedar Abhyankar on 6/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
