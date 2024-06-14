653d8b96de41edb9a52ca903be02c767e9b45163
Kedar Abhyankar <krabhyankar@gmail.com>
2024-06-13 21:54:28 -0700

    Initial Firebase push + added .gitignore detailing firebase plist

--- Barbell/BarbellApp.swift
+++ Barbell/BarbellApp.swift
@@ -6,9 +6,22 @@
 //
 
 import SwiftUI
+import Firebase
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions:  [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Booting Firebase")
+        FirebaseApp.configure()
+        return true
+    }
+    
+}
 @main
 struct BarbellApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
